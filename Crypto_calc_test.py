e248e8c502cddf9468ce0855f2f14dac00adc7ce
Buza15 <Byza15@gmail.com>
2023-02-19 01:12:43 +0300

    final

--- Crypto_calc_test.py
+++ Crypto_calc_test.py
@@ -1,5 +1,31 @@
+import requests
+
+from binance.client import Client
+
+# Enter your Binance API keys here
+api_key = 'YOUR_API_KEY'
+api_secret = 'YOUR_API_SECRET'
+
+# Initialize the Binance client with your API keys
+client = Client(api_key, api_secret)
+
+# Replace 'CRYPTO_SYMBOL' with the symbol of the crypto token you want to get the price for, e.g. 'BTCUSDT', 'ETHUSDT', etc.
+
+crypto_symbol = input("Выбираем крипту:") + "USDT"
+
+# Get the current price from Binance API
+ticker = client.get_symbol_ticker(symbol=crypto_symbol)
+
+# Get the price in USDT
+current_price = ticker['price']
+
+print(f"The current price of {crypto_symbol} is {current_price}")
+
+to_calculate = current_price
+
 value = float(input("На сколько $ покупаем? :"))
-curr_price = float(input("Текущая цена монеты:"))
+
+curr_price = float(to_calculate)
 
 curr_result = value / curr_price
 
@@ -13,8 +39,14 @@ next = input()
 if next == "-":
  print("OK")
 else:
- new_price = int(input("Новая цена монеты:"))
+ new_price = float(input("Новая цена монеты:"))
  new_vlaue = curr_result * new_price
- print("Можно продать за", new_vlaue, "$")
- print("Вы вложили", value, "$ и получили", new_vlaue, "$")
- print("Чистая прибыль составила", new_vlaue - value, "$")
+ profit = new_vlaue - value
+ if profit > 0:
+  print("Можно продать за", new_vlaue, "$")
+  print("Вы вложили", value, "$ и получили", new_vlaue, "$")
+  print("Чистая прибыль составила", profit, "$")
+ else:
+  print("Можно продать за", new_vlaue, "$")
+  print("Вы вложили", value, "$ и остаток составил", new_vlaue, "$")
+  print("Убыток составил", profit, "$")
