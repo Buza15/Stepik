ca8f65a7fb6a652d083bc3436f35cdce96246dc6
Buza15 <Byza15@gmail.com>
2023-03-09 21:37:00 +0300

    main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 # here I'll test everything regarding Git
 
-a = int(input("Enter a number:"))
+a = int(input())
 
-b = a + 2
+if a % 1000 == 0 and a % 100 == 0:
+    print("YES")
+else:
+    print("NO")
 
-print("Your number + 2 =", b)
